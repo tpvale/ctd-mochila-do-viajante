b1603a32ff3d07daa50820dbf68868c3346fa5ba
tpvale <tpv.tiago@gmail.com>
2022-03-08 20:49:52 -0300

    ctd-t05-feii mesa-aula-05

---
+++ ctd-t05-feii/mesa-aula-05/scripts/app.js
@@ -0,0 +1,19 @@
+function darkMode() {
+  let backgroundDark = document.querySelector('body');
+  backgroundDark.classList.toggle('backgroundDark');
+
+  let titleDark = document.querySelector('h1');
+  titleDark.classList.toggle('titleDark');
+  
+  let btTextDark = document.querySelector('h3');
+  btTextDark.classList.toggle('btTextDark');
+
+  let cardsDark = document.querySelectorAll('.item');
+
+  cardsDark.forEach (element => {element.classList.toggle('cardsDark')});
+
+  let cardTextDark = document.querySelectorAll('.item h2, p');
+  cardTextDark.forEach (element => {element.classList.toggle('cardTextDark')}); 
+ }
+var btSwitch = document.getElementById("btSwitch");
+btSwitch.addEventListener("click", darkMode);
